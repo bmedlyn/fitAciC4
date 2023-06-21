fbec7196c9e9c53449e2f0dc4f34dbb9bb8eaab9
bmedlyn <b.medlyn@westernsydney.edu.au>
2023-06-21 19:46:26 +1000

    Fixed bug in fit_ains where I fit Vpmax to whole curve not just low Ci

--- R/fit_ains.R
+++ R/fit_ains.R
@@ -1,5 +1,4 @@
 
-
 # implement fitting routine according to Ainsworth and Leakey
 # this fits Vpmax to initial slope (Ci < 150 ppm) - assumes sufficient points
 # Vcmax is fitted to the whole curve
@@ -23,7 +22,7 @@ fitAciC4_ainsy <- function(data,lowCi = 150) {
   Kp <- 80
   fit_low <- try(nls(Photo ~ vpf(Ci=Ci,Vpmax,Kp,Rm),
                  start=list(Vpmax=25,Rm=0.5),trace=FALSE,
-                 control=(nls.control(warnOnly=TRUE)),data = data))
+                 control=(nls.control(warnOnly=TRUE)),data = low))
   
   # fit 4 parameters to full curve
   fit_all <- try(nls(Photo ~ non_rect_hyp(Ci=Ci,Amax,alpha,theta=0.7,Rd),
@@ -34,15 +33,19 @@ fitAciC4_ainsy <- function(data,lowCi = 150) {
   ret2 <- coef(fit_all)
   ret <- c(ret1,ret2)
   
-  Ciseq <- seq(20,max(data$Ci),by=20)
+  #Ciseq <- seq(20,max(data$Ci),by=20)
+  Ciseq <- seq(-100,1500,by=20)
   fittedAvp <- with(data, vpf(Ci=Ciseq,Vpmax=ret["Vpmax"],Kp,Rm=ret["Rm"]))
   fittedAvc <- with(data,non_rect_hyp(Ciseq,Amax=ret["Amax"],
                                        alpha=ret["alpha"],theta=0.7,Rd=ret["Rd"]))
   title <- paste0("Ains ",data$ID[1],
                   " Vpmax ",round(ret["Vpmax"],1)," Amax ",round(ret["Amax"],1))
-  with(data,plot(Ci,Photo,ylim=c(0,2*max(Photo)),main=title))
+  with(data,plot(Ci,Photo,ylim=c(-10,1.2*max(Photo)),main=title,
+                 xlim=c(-100,1500)))
   points(Ciseq,fittedAvc,col="red",type="l")
   points(Ciseq,fittedAvp,col="blue",type="l") 
+  abline(v=0,lty=2)
+  abline(v=150,lty=2)
   return(ret)
   
 }
